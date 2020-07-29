4ce4386808dbb0b53cc0b46c1c3f40d0745d60cf
max <m.gachanga97@gmail.com>
2020-07-29 12:24:00 +0300

    dsfkdsfdsf

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -17,6 +17,7 @@ class CheckoutController extends Controller
 {
     public function index(){
         $getIp = UserSystemInfoHelper::get_ip();
+        dd($getIp);
 
         $checkouts = Checkout::where('ip', $getIp)->get();
             $checks = Checkout::where('ip', $getIp)->get();
