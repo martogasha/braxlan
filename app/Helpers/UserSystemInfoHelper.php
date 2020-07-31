43f888a8000d65093683f13c7e892ce29d273ef2
max <m.gachanga97@gmail.com>
2020-07-31 08:18:34 +0300

    dfdsfdsfvdss

--- app/Helpers/UserSystemInfoHelper.php
+++ app/Helpers/UserSystemInfoHelper.php
@@ -4,20 +4,27 @@
  *
  */
 namespace App\Helpers;
-use Illuminate\Http\Request;
-
-
 class UserSystemInfoHelper
 {
     private static function get_user_agent(){
         return $_SERVER['HTTP_USER_AGENT'];
     }
 
-    public static function get_ip(Request $request){
+    public static function get_ip(){
 
         $ipaddress = '';
         if (isset($_SERVER['SERVER_ADDR']))
-            $ipaddress =$request->ip();
+            $ipaddress = $_SERVER['SERVER_ADDR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
         else
             $ipaddress = 'UNKNOWN';
         return $ipaddress;
