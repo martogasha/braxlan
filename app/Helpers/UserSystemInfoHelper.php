21c438457ee79bc39c95ba326d4c708f79c04983
max <m.gachanga97@gmail.com>
2020-08-01 12:27:31 +0300

    dfdsfdsdsacascas

--- app/Helpers/UserSystemInfoHelper.php
+++ app/Helpers/UserSystemInfoHelper.php
@@ -4,6 +4,8 @@
  *
  */
 namespace App\Helpers;
+use NazmulB\MacAddressPhpLib\MacAddress;
+
 class UserSystemInfoHelper
 {
     private static function get_user_agent(){
@@ -12,22 +14,7 @@ class UserSystemInfoHelper
 
     public static function get_ip(){
 
-        $ipaddress = '';
-        if (isset($_SERVER['HTTP_CLIENT_IP']))
-            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED']))
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_FORWARDED']))
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        else if(isset($_SERVER['REMOTE_ADDR']))
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
-        else
-            $ipaddress = 'UNKNOWN';
-        return $ipaddress;
+      return MacAddress::getMacAddress();
 
     }
 
